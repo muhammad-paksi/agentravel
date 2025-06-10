bee4635a7da5110e628dfd0cc02cec853ef9bddf
muhammad-paksi <paksi.home@gmail.com>
2025-06-10 20:11:21 +0700

    Update all.js

--- database/model/all.js
+++ database/model/all.js
@@ -23,30 +23,15 @@ const reservasiSchema = new mongoose.Schema(
     nik              : { type: Number, required: true },
     name             : { type: String, required: true },
     contact          : { type: String, required: true },
-    type             : { type: String, enum: ['flight', 'hotel', 'activity'], required: false },
     ticket_id        : { type: Number, required: true },
     destination      : { type: String, required: true },
-    departure_date   : { type: Date, required: true },
-    transport_type   : { type: String, enum: ['Plane', 'Ship', 'Train', 'Bus'], required: true},
-    carrier_name     : { type: String, required: true },
-    ticket_price     : { type: Number, required: true },
-    // 5 variabel dibawah untuk menambah reservasi untuk hotel juga jika iya setuju, jika tidak maka disable
-    total_persons    : { type: Number }, // Jumlah orang yang dipesan
-    checkInDate      : { type: Date, required: false },       
-    estimated_budget : { type: Number, required: false },
-    hotel_name       : { type: String, required: false },
-    room_price     : { type: Number, required: false },
-    // total_price menyimpan hasil ticket_price + room_price, jika hotel tidak ada maka total_price = ticket_price
+    date             : { type: Date, required: true },
+    estimated_budget : { type: Number, required: true },
     total_price      : { type: Number, required: true },
-    
-    payment_method   : { type: String, enum: ['Prepaid', 'Postpaid'], required: true },  
+    payment_method   : { type: String, enum: ['Prepaid', 'Postpaid'], required: true },
     payment_status   : { type: String, enum: ['Pending', 'Paid'], required: true },
     status           : { type: String, enum: ['Booked', 'Completed', 'Canceled'], required: true },
-
-    admin_id         : [{ /* type: String,*/ 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'pengguna', 
-      required: true }],
+    admin_id         : { type: String, required: true },
   },
   { collection: "reservasi", timestamps: true },
 );
